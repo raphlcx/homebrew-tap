ceaf7350e8ddacbad13d08a8be57664722d510ba
Ling Cong Xiang <ralcx@outlook.com>
2022-07-03 10:26:44 +0800

    vim-minimal: Update to 9.0.0000

--- Formula/vim-minimal.rb
+++ Formula/vim-minimal.rb
@@ -2,8 +2,8 @@ class VimMinimal < Formula
   desc "Minimal vim with no interpreter interfaces"
   homepage "https://www.vim.org/"
   # vim should only be updated every 50 releases on multiples of 50
-  url "https://github.com/vim/vim/archive/v8.2.5100.tar.gz"
-  sha256 "d81cddd32124b5a9e2a7c6f5cc5b64529f9a36c9aec1cb958d2398ad1c353d04"
+  url "https://github.com/vim/vim/archive/v9.0.0000.tar.gz"
+  sha256 "1b3cd3732eb7039cf58a9321de26ab1a12d81c2f6760eb03c5d7b60d548f4587"
   license "Vim"
   head "https://github.com/vim/vim.git", branch: "master"
 
