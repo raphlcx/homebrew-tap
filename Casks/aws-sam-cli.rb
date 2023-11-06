4a15863bc6b28378cdbc9ff33ac208bae08404ff
Ling Cong Xiang <ralcx@outlook.com>
2023-11-06 14:02:09 +0800

    Update packages
      - vim-minimal 9.0.2050
      - awscli 2.13.32
      - aws-sam-cli 1.100.0

--- Casks/aws-sam-cli.rb
+++ Casks/aws-sam-cli.rb
@@ -1,6 +1,6 @@
 cask "aws-sam-cli" do
-  version "1.98.0"
-  sha256 "401480ad9ccf3bdf4298b7111e22babbb4fafaf336f60eabdd1735dd1a43fce7"
+  version "1.100.0"
+  sha256 "604dca05d13dac09e8343b8099c19fe844d74cda6ff065e622717f09f7cd59de"
 
   url "https://github.com/aws/aws-sam-cli/releases/download/v#{version}/aws-sam-cli-macos-arm64.pkg"
   name "AWS Serverless Application Model (AWS SAM) Command Line Interface"
--- Casks/awscli.rb
+++ Casks/awscli.rb
@@ -1,6 +1,6 @@
 cask "awscli" do
-  version "2.13.25"
-  sha256 "35ed7356c61bf13f63a775ee8a8286fdbc643f7b31e22dee60aff3d0ae93abab"
+  version "2.13.32"
+  sha256 "172804fb946a2dd13e4002c3acfde275ca443a512e8bc3df7c21368190f27f23"
 
   url "https://awscli.amazonaws.com/AWSCLIV2-#{version}.pkg"
   name "AWS Command Line Interface"
