2e5979db7fc3d5520f96d666ab4fc2421b51ba06
Ling Cong Xiang <ralcx@outlook.com>
2024-11-11 23:05:05 +0800

    cask: Amend mpv-nightly metadata
    Changes:
    
      - Use "mpv" as the name, regardless of whether it is a nightly build
      - Add man page
      - Add zap stanza

--- Casks/mpv-nightly.rb
+++ Casks/mpv-nightly.rb
@@ -3,7 +3,7 @@ cask "mpv-nightly" do
   sha256 "f3698808a126b682dc8eefcdd0c89e710c05b339f246a4218760e3630dd0cc05"
 
   url "https://nightly.link/mpv-player/mpv/workflows/build/master/mpv-macos-15-arm.zip"
-  name "mpv-nightly"
+  name "mpv"
   desc "Media player based on MPlayer and mplayer2"
   homepage "https://mpv.io/"
 
@@ -11,4 +11,12 @@ cask "mpv-nightly" do
 
   app "mpv.app"
   binary "#{appdir}/mpv.app/Contents/MacOS/mpv"
+  manpage "documentation/man/mpv.1"
+
+  zap trash: [
+    "~/.config/mpv",
+    "~/Library/Logs/mpv.log",
+    "~/Library/Preferences/io.mpv.plist",
+    "~/Library/Preferences/mpv.plist",
+  ]
 end
