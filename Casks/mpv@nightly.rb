7f38141d115f3451722350d875ad6aab9492d73a
Ling Cong Xiang <ralcx@outlook.com>
2026-02-19 01:42:05 +0800

    Update packages
    Casks:
    
      - mpv@nightly 22038346825

--- Casks/mpv@nightly.rb
+++ Casks/mpv@nightly.rb
@@ -1,8 +1,8 @@
 cask "mpv@nightly" do
-  version "17750851503"
-  sha256 "f59a2957759b1c5e250f38b46c6c1fcce072a8b8963442e538039b7951220eec"
+  version "22038346825"
+  sha256 "2891a4de39558ebdc24cc5bf66640089ba5d38a309679f7d671b73d71aba6b70"
 
-  url "https://nightly.link/mpv-player/mpv/workflows/build/master/mpv-macos-15-arm.zip"
+  url "https://nightly.link/mpv-player/mpv/workflows/build/master/mpv-v0.41.0-dev-gaa8221372-#{version}-macos-26-arm.zip"
   name "mpv"
   desc "Media player based on MPlayer and mplayer2"
   homepage "https://mpv.io/"
